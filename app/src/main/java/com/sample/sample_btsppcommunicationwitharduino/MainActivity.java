dc8d406ddb40be77636fa9a7ce0343ed5dbb09a6
tayumo <tayumo.dev@gmail.com>
2021-12-20 09:37:00 +0900

    add TAG for MainActivity

--- app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
+++ app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
