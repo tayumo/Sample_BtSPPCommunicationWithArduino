ed0e9d0d390977daee274550da04bdd8c14bbf42
tayumo <tayumo.dev@gmail.com>
2021-12-20 09:50:06 +0900

    check Bluetooth enabled

--- app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
+++ app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
@@ -1,15 +1,75 @@
 package com.sample.sample_btsppcommunicationwitharduino;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private ActivityResultLauncher<Intent> mActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkDeviceSupportBluetooth();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                        int resultCode = result.getResultCode();
+
+                        switch (resultCode) {
+                            case RESULT_OK:
+                                break;
+                            case RESULT_CANCELED:
+                                showBluetoothDisabledAlertDialogAndFinishApp();
+                                break;
+                            case RESULT_FIRST_USER:
+                            default:
+                                break;
+                        }
+                    }
+                });
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            mActivityResultLauncher.launch(enableBtIntent);
+        }
+    }
+
+    private void checkDeviceSupportBluetooth() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.e(TAG, "Device does not support Bluetooth.");
+            finish();
+        }
+    }
+
+    private void showBluetoothDisabledAlertDialogAndFinishApp() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("BluetoothがOFFになっています。")
+                .setMessage("BluetoothをONにしてアプリを再起動してください。")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
     }
 }
