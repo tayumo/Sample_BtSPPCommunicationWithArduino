ac51feabb8fc56d99e63fb799bc1cc13f35fca5f
tayumo <tayumo.dev@gmail.com>
2021-12-20 10:44:30 +0900

    implement AddDeviceButton

--- app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
+++ app/src/main/java/com/sample/sample_btsppcommunicationwitharduino/MainActivity.java
@@ -12,6 +12,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,10 +21,21 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter mBluetoothAdapter;
     private ActivityResultLauncher<Intent> mActivityResultLauncher;
 
+    private Button mAddDeviceButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddDeviceButton = findViewById(R.id.add_device_button);
+        mAddDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                startActivity(intent);
+            }
+        });
 
         checkDeviceSupportBluetooth();
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
